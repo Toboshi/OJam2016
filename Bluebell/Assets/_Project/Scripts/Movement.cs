154ed21da204decb79051a105fbe9fbf6fbe5a44
Ameliane <Ameliane@users.noreply.github.com>
2016-06-25 16:25:19 -0400

    Added Pause state and screen, and splash screen.

--- Bluebell/Assets/_Project/Scripts/Movement.cs
+++ Bluebell/Assets/_Project/Scripts/Movement.cs
@@ -15,6 +15,9 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (UIManager.Instance.IsGamePaused())
+            return;
+
         this.transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * Time.deltaTime * m_Speed;
         if (Input.GetAxis("Horizontal") < 0)
             GetComponentInChildren<SpriteRenderer>().flipX = true;
--- Bluebell/Assets/_Project/Scripts/UIManager.cs
+++ Bluebell/Assets/_Project/Scripts/UIManager.cs
@@ -8,11 +8,19 @@ public class UIManager : MonoBehaviour
 
     public float m_TransitionTime = 2;
 
+    public GameObject m_SplashScreenCanvas;
+    public Image m_SplashScreen;
+    public GameObject m_Title;
+
     public GameObject m_PauseCanvas;
+    public Slider m_GameSound;
+    public Slider m_AmbientSound;
 
     public GameObject m_EndCanvas;
     public Image m_EndBackground;
     public GameObject m_EndCredits;
+
+    private bool m_GamePaused = true;
 
     // Use this for initialization
     void Start()
@@ -21,20 +29,56 @@ public class UIManager : MonoBehaviour
             Instance = this;
         else
             Destroy(this);
+
+        StartCoroutine(StartGame_cr());
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
     }
 
-    [ContextMenu("The End")]
+    public bool IsGamePaused()
+    {
+        return m_GamePaused;
+    }
+
+    public void Pause()
+    {
+        m_GamePaused = !m_GamePaused;
+        m_PauseCanvas.SetActive(m_GamePaused);
+    }
+
     public void EndGame()
     {
         m_EndCanvas.SetActive(true);
 
         StartCoroutine(TheEnd_cr());        
+    }
+
+    IEnumerator StartGame_cr()
+    {
+        Color c = Color.white;
+        c.a = 1;
+
+        while (c.a > 0)
+        {
+            c.a -= Time.deltaTime / m_TransitionTime;
+            m_SplashScreen.color = c;
+            yield return null;
+        }
+
+        c.a = 0;
+        m_EndBackground.color = c;
+
+        yield return null;
+
+        m_Title.SetActive(false);
+        m_GamePaused = false;
     }
 
     IEnumerator TheEnd_cr()
