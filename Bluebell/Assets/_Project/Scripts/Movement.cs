3ab63b63e521eda5d85b22f22fd91a1b67acc110
Toboshi <Toboshi@users.noreply.github.com>
2016-06-25 20:16:09 -0400

    Better movement

--- Bluebell/Assets/_Project/Scripts/Movement.cs
+++ Bluebell/Assets/_Project/Scripts/Movement.cs
@@ -3,20 +3,19 @@ using System.Collections;
 
 public class Movement : MonoBehaviour
 {
-
     public float m_Speed = 5f;
 
     public Animator m_Anim;
 
     private bool m_IsMoving = false;
 
-    // Use this for initialization
+    Rigidbody2D m_Body;
+    
     void Start()
     {
-
+        m_Body = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (UIManager.Instance.IsGamePaused())
@@ -30,6 +29,14 @@ public class Movement : MonoBehaviour
             GetComponentInChildren<SpriteRenderer>().flipX = true;
         else if (Input.GetAxis("Horizontal") > 0)
             GetComponentInChildren<SpriteRenderer>().flipX = false;
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            m_Body.velocity = Vector2.Scale(m_Body.velocity, Vector2.right);
+            m_Body.gravityScale = 0;
+        }
+        else
+            m_Body.gravityScale = 2;
 
         // Animation if you've just started or stopped moving
         if(input != Vector3.zero && !m_IsMoving)
